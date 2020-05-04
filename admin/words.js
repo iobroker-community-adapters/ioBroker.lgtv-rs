8ced1f9033081d7dbcd0ac68f5f0580dbf8c477f
instalator <vvvalt@mail.ru>
2020-05-04 17:26:07 +0700

    0.1.0

--- admin/words.js
+++ admin/words.js
@@ -1,12 +1,35 @@
-// Dictionary (systemDictionary is global variable from adapter-settings.js)
+/*global systemDictionary:true */
+'use strict';
 systemDictionary = {
-	"LG TV adapter settings": {"de": "LG TV adapter settings", "ru": "LG TV adapter settings"},
-	"host":                   	{"en": "host", "de": "host", "ru": "host"},
-	"port":                   	{"en": "port", "de": "port", "ru": "port"},
-	"login":                   	{"en": "login", "de": "login", "ru": "login"},
-	"password":                	{"en": "password", "de": "password", "ru": "password"},
-	"on save adapter restarts with new config immediately": {
-		"de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
-		"ru": "При сохранении настроек адаптера он сразу же перезапускается"
-	}
+    "LG TV adapter settings":                               {"de": "LG TV adapter settings", "ru": "LG TV adapter settings"},
+    "host":                                                 {
+        "en":    "IP address",
+        "de":    "IP address",
+        "ru":    "IP Адрес",
+        "pt":    "IP address",
+        "nl":    "IP address",
+        "fr":    "IP address",
+        "it":    "IP address",
+        "es":    "IP address",
+        "pl":    "IZ address",
+        "zh-cn": "IP address"
+    },
+    "port":                                                 {
+        "en":    "Port",
+        "de":    "Port",
+        "ru":    "Порт",
+        "pt":    "Port",
+        "nl":    "Port",
+        "fr":    "Port",
+        "it":    "Port",
+        "es":    "Port",
+        "pl":    "Port",
+        "zh-cn": "口"
+    },
+    "login":                                                {"en": "login", "de": "login", "ru": "login"},
+    "password":                                             {"en": "password", "de": "password", "ru": "password"},
+    "on save adapter restarts with new config immediately": {
+        "de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
+        "ru": "При сохранении настроек адаптера он сразу же перезапускается"
+    }
 };
--- main.js
+++ main.js
@@ -1,112 +1,93 @@
 "use strict";
-
-var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
-var adapter = utils.adapter('lgtv-rs');
-var net = require('net');
-var lgtv_commands = require(__dirname + '/admin/commands.json'),
+const utils = require('@iobroker/adapter-core');
+let net = require('net');
+let lgtv_commands = require(__dirname + '/admin/commands.json'),
     COMMANDS = lgtv_commands.commands,
     COMMAND_MAPPINGS = lgtv_commands.command_mappings,
     VALUE_MAPPINGS = lgtv_commands.value_mappings,
     REMOTE_CMDS = lgtv_commands.remote;
-var lgtv, recnt, connection = false;
-var query = null;
-var tabu = false;
-var polling_time = 5000;
-var states = {}, old_states = {};
-var querycmd = [];
-var remote_command_send = false;
+let adapter, lgtv, recnt, connection = false, query = null, tabu = false, polling_time = 5000, states = {}, old_states = {}, querycmd = [], remote_command_send = false;
 
-adapter.on('unload', function (callback) {
-    if(lgtv){
-        lgtv.destroy();
-    }
-    try {
-        adapter.log.info('cleaned everything up...');
-        callback();
-    } catch (e) {
-        callback();
-    }
-});
-
-adapter.on('objectChange', function (id, obj) {
-    // Warning, obj can be null if it was deleted
-    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
-});
-
-adapter.on('stateChange', function (id, state) {
-    if (connection){
-        if (state && !state.ack) {
-            tabu = true;
-            adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));
-            var ids = id.split(".");
-            var name = ids[ids.length - 2].toString();
-            var command = ids[ids.length - 1].toString();
-            var val = [state.val];
-            if (state.val === false || state.val === 'false'){
-                val = 'off';
-            } else if (state.val === true || state.val === 'true'){
-                val = 'on';
+function startAdapter(options){
+    return adapter = utils.adapter(Object.assign({}, options, {
+        systemConfig: true,
+        name:         'lgtv-rs',
+        ready:        main,
+        unload:       callback => {
+            query && clearInterval(query);
+            recnt && clearTimeout(recnt);
+            /*timeoutPoll && clearTimeout(timeoutPoll);
+            reconnectTimeOut && clearTimeout(reconnectTimeOut);
+            timeout && clearTimeout(timeout);*/
+            try {
+                lgtv && lgtv.destroy();
+                adapter.log.debug('cleaned everything up...');
+                callback();
+            } catch (e) {
+                callback();
             }
-
-            var cmd = COMMAND_MAPPINGS[command];
-            var key;
-            if(name == 'remote'){
-				remote_command_send = true;
-                key = 'mc 00 ' + REMOTE_CMDS[command];
-                send(key);
-            } else {
-                if (cmd){
-                    if(VALUE_MAPPINGS[cmd][command]){
-                        if(~VALUE_MAPPINGS[cmd][command]['value'].indexOf(',')){
-                            key = cmd + ' 00 ' + (parseInt(val)).toString(16);
-                        }
+        },
+        stateChange:  (id, state) => {
+            if (connection){
+                if (id && state && !state.ack){
+                    adapter.log.debug(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+                    tabu = true;
+                    let ids = id.split(".");
+                    let name = ids[ids.length - 2].toString();
+                    let command = ids[ids.length - 1].toString();
+                    let val = [state.val];
+                    if (state.val === false || state.val === 'false'){
+                        val = 'off';
+                    } else if (state.val === true || state.val === 'true'){
+                        val = 'on';
+                    }
+                    let cmd = COMMAND_MAPPINGS[command];
+                    let key;
+                    if (name === 'remote'){
+                        remote_command_send = true;
+                        key = 'mc 00 ' + REMOTE_CMDS[command];
+                        send(key);
                     } else {
-                        if(VALUE_MAPPINGS[cmd][val]['value']){
-                            key = cmd + ' 00 ' + VALUE_MAPPINGS[cmd][val]['value'];
+                        if (cmd){
+                            if (VALUE_MAPPINGS[cmd][command]){
+                                if (~VALUE_MAPPINGS[cmd][command]['value'].indexOf(',')){
+                                    key = cmd + ' 00 ' + (parseInt(val)).toString(16);
+                                }
+                            } else {
+                                if (VALUE_MAPPINGS[cmd][val]['value']){
+                                    key = cmd + ' 00 ' + VALUE_MAPPINGS[cmd][val]['value'];
+                                }
+                            }
+                            send(key);
+                        } else {
+                            adapter.log.error('Error command ' + cmd);
                         }
                     }
-                    send(key);
-                } else {
-                    adapter.log.error('Error command ' + cmd);
                 }
+            } else {
+                adapter.log.debug('Send command error - MPD NOT connected!');
             }
         }
-    }
-});
-
-adapter.on('message', function (obj) {
-    if (typeof obj == 'object' && obj.message) {
-        if (obj.command == 'send') {
-            console.log('send command');
-            if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-        }
-    }
-});
-
-adapter.on('ready', function () {
-    main();
-    CreatObject();
-    //json();
-});
-
+    }));
+}
 
 function json(){
-    var cmd_mappings = {};
-    var val_mappings = {};
-    var key;
-    var file = lgtv_commands;
+    let cmd_mappings = {};
+    let val_mappings = {};
+    let key;
+    let file = lgtv_commands;
     for (key in COMMANDS) {
-        if(COMMANDS.hasOwnProperty(key)){
+        if (COMMANDS.hasOwnProperty(key)){
             cmd_mappings[COMMANDS[key]['name']] = key;
         }
     }
     for (key in COMMANDS) {
-        if(COMMANDS.hasOwnProperty(key)){
-            //adapter.log.info('(var key in COMMANDS) key: ' + key);
+        if (COMMANDS.hasOwnProperty(key)){
+            //adapter.log.info('(let key in COMMANDS) key: ' + key);
             val_mappings[key] = {};
-            for (var k in COMMANDS[key]['values']) {
-                if(COMMANDS[key]['values'].hasOwnProperty(k)){
-                    //adapter.log.info('(var k in COMMANDS[key][values]) k: ' + k);
+            for (let k in COMMANDS[key]['values']) {
+                if (COMMANDS[key]['values'].hasOwnProperty(k)){
+                    //adapter.log.info('(let k in COMMANDS[key][values]) k: ' + k);
                     val_mappings[key][COMMANDS[key]['values'][k]['name']] = {};
                     val_mappings[key][COMMANDS[key]['values'][k]['name']]['value'] = k;
                     val_mappings[key][COMMANDS[key]['values'][k]['name']]['models'] = COMMANDS[key]['values'][k]['models'];
@@ -118,10 +99,11 @@ function json(){
     file.value_mappings = val_mappings;
 }
 
-function main() {
+function main(){
     adapter.subscribeStates('*');
-    for (var key in COMMANDS) {
-        if(COMMANDS.hasOwnProperty(key)){
+    CreatObject();
+    for (let key in COMMANDS) {
+        if (COMMANDS.hasOwnProperty(key)){
             if (COMMANDS[key].values.hasOwnProperty('ff') && COMMANDS[key]['name'] !== 'power'){
                 querycmd.push(key + ' 00 ' + 'ff');
             }
@@ -132,49 +114,51 @@ function main() {
 
 function connect(cb){
     //adapter.config.host = '192.168.1.56';
-    var in_msg = '';
-    var host = adapter.config.host ? adapter.config.host : '192.168.1.56';
-    var port = adapter.config.port ? adapter.config.port : 23;
+    let in_msg = '';
+    let host = adapter.config.host ? adapter.config.host :'192.168.1.56';
+    let port = adapter.config.port ? adapter.config.port :23;
     adapter.log.debug('LG TV ' + 'connect to: ' + host + ':' + port);
-    var c = COMMAND_MAPPINGS['power'];
-    var q = VALUE_MAPPINGS[c]['query']['value'];
-    var check_cmd = c + ' 00 ' + q;
-    lgtv = net.connect(port, host, function() {
+    let c = COMMAND_MAPPINGS['power'];
+    let q = VALUE_MAPPINGS[c]['query']['value'];
+    let check_cmd = c + ' 00 ' + q;
+    lgtv = net.connect(port, host, () => {
         adapter.setState('info.connection', true, true);
         adapter.log.info('LG TV connected to: ' + host + ':' + port);
         connection = true;
         clearInterval(query);
-        query = setInterval(function() {
-            if(!tabu){
-               send(check_cmd);
+        query = setInterval(() => {
+            if (!tabu){
+                send(check_cmd);
             }
         }, polling_time);
-        if(cb){cb();}
+        if (cb){
+            cb();
+        }
     });
-    lgtv.on('data', function(chunk) {
+    lgtv.on('data', (chunk) => {
         in_msg += chunk;
-        if(in_msg[9] =='x'){
-            if(in_msg.length > 10){
-                in_msg = in_msg.substring(0,10);
+        if (in_msg[9] === 'x'){
+            if (in_msg.length > 10){
+                in_msg = in_msg.substring(0, 10);
             }
             adapter.log.debug("LG TV incomming: " + in_msg);
             parse(in_msg);
             in_msg = '';
         }
-        if(in_msg.length > 15){
+        if (in_msg.length > 15){
             in_msg = '';
         }
     });
 
-    lgtv.on('error', function(e) {
-        if (e.code == "ENOTFOUND" || e.code == "ECONNREFUSED" || e.code == "ETIMEDOUT") {
+    lgtv.on('error', (e) => {
+        if (e.code === "ENOTFOUND" || e.code === "ECONNREFUSED" || e.code === "ETIMEDOUT"){
             lgtv.destroy();
         }
         err(e);
     });
 
-    lgtv.on('close', function(e) {
-        if(connection){
+    lgtv.on('close', (e) => {
+        if (connection){
             err('LG TV disconnected');
         }
         reconnect();
@@ -182,30 +166,30 @@ function connect(cb){
 }
 
 function parse(msg){
-    var req = {};
-    if (msg[msg.length - 1] == 'x'){
+    let req = {};
+    if (msg[msg.length - 1] === 'x'){
         req.cmd = msg[0];
-        req.id  = msg[2] + msg[3];
+        req.id = msg[2] + msg[3];
         req.ack = getack(msg[5] + msg[6]);
         req.val = msg[7] + msg[8];
     }
     adapter.log.debug("req: " + JSON.stringify(req));
-    if(req.ack){
-        var val;
-        for (var key in COMMANDS) {
-            if(COMMANDS.hasOwnProperty(key)){
-                if (key[1] == req.cmd && !remote_command_send){
-                    var obj = COMMANDS[key].name;
+    if (req.ack){
+        let val;
+        for (let key in COMMANDS) {
+            if (COMMANDS.hasOwnProperty(key)){
+                if (key[1] === req.cmd && !remote_command_send){
+                    let obj = COMMANDS[key].name;
                     if (VALUE_MAPPINGS[key][obj]){
                         if (~VALUE_MAPPINGS[key][obj]['value'].indexOf(',')){
                             val = parseInt(req.val, 16);
                         }
                     } else {
-						if (COMMANDS[key]['values'][req.val]['name']){
-							val = COMMANDS[key]['values'][req.val]['name'];
-						} else {
-							adapter.log.error("Error not found name in " + COMMANDS[key]['values'][req.val]);
-						} 
+                        if (COMMANDS[key]['values'][req.val]['name']){
+                            val = COMMANDS[key]['values'][req.val]['name'];
+                        } else {
+                            adapter.log.error("Error not found name in " + COMMANDS[key]['values'][req.val]);
+                        }
                     }
                     states[obj] = toBool(val);
                     //adapter.log.debug("obj:" + val);
@@ -213,41 +197,41 @@ function parse(msg){
                         old_states[obj] = states[obj];
                         setObject(obj, states[obj]);
                     }
-                    if (obj == 'power'){
+                    if (obj === 'power'){
                         //states[obj] = true; //for debug
-                        if (states[obj] == true){
+                        if (states[obj] === true){
                             get_commands();
                         }
                     }
-                } else if (remote_command_send && key[1] == req.cmd){
-					remote_command_send = false;
-					for (var key in REMOTE_CMDS) {
-						if(REMOTE_CMDS.hasOwnProperty(key)){
-							if(REMOTE_CMDS[key] == req.val){
-								adapter.log.debug("Remote cmd OK! key: { " + key + " } key, val: {" + req.val + "}");
-								break;
-							}
-						}
-					}
-				}
+                } else if (remote_command_send && key[1] === req.cmd){
+                    remote_command_send = false;
+                    for (let key in REMOTE_CMDS) {
+                        if (REMOTE_CMDS.hasOwnProperty(key)){
+                            if (REMOTE_CMDS[key] === req.val){
+                                adapter.log.debug("Remote cmd OK! key: { " + key + " } key, val: {" + req.val + "}");
+                                break;
+                            }
+                        }
+                    }
+                }
             }
         }
         //adapter.log.debug("states:" + JSON.stringify(states));
-    } else if (req.val == '00'){
+    } else if (req.val === '00'){
         adapter.log.debug('Illegal Code');
     }
 }
 
 function get_commands(){
     tabu = true;
-    var tm = 5000;
-    var interval;
-    setTimeout(function (){
+    let tm = 5000;
+    let interval;
+    setTimeout(() => {
         tabu = false;
     }, (querycmd.length * tm));
-    querycmd.forEach(function(cmd, i, arr) {
+    querycmd.forEach((cmd, i, arr) => {
         interval = tm * i;
-        setTimeout(function() {
+        setTimeout(() => {
             cmd = cmd + '\n\r';
             adapter.log.debug('Send Command: ' + cmd);
             lgtv.write(cmd);
@@ -265,15 +249,15 @@ function send(cmd){
 }
 
 function setObject(name, val){
-    var type = 'string';
-    var role = 'media';
+    let type = 'string';
+    let role = 'media';
     adapter.log.debug('name:' + name);
-    var odj_cmd = COMMANDS[COMMAND_MAPPINGS[name]];
-    var obj_val;
-    if(VALUE_MAPPINGS[COMMAND_MAPPINGS[name]]){
+    let odj_cmd = COMMANDS[COMMAND_MAPPINGS[name]];
+    let obj_val;
+    if (VALUE_MAPPINGS[COMMAND_MAPPINGS[name]]){
         obj_val = VALUE_MAPPINGS[COMMAND_MAPPINGS[name]];
         //adapter.log.debug('odj_cmd:' + JSON.stringify(odj_cmd));
-        adapter.getState(odj_cmd, function (err, state){
+        adapter.getObject(odj_cmd, (err, state) => {
             if (odj_cmd){
                 if ((err || !state) && odj_cmd.hasOwnProperty('description')){
                     if (odj_cmd.hasOwnProperty('values')){
@@ -306,31 +290,27 @@ function setObject(name, val){
 }
 
 function toBool(s){
-    if(s == 'on'){
+    if (s === 'on'){
         s = true;
-    } else if (s == 'off'){
+    } else if (s === 'off'){
         s = false;
     }
     return s;
 }
 
 function getack(s){
-    if(s == 'OK'){
-        s = true;
-    } else {
-        s = false;
-    }
+    s = s === 'OK';
     return s;
 }
 
 function reconnect(){
-    clearInterval(query);
-    clearTimeout(recnt);
+    query && clearInterval(query);
+    recnt && clearTimeout(recnt);
     lgtv.destroy();
     adapter.setState('info.connection', false, true);
     adapter.log.info('Reconnect after 60 sec...');
     connection = false;
-    recnt = setTimeout(function() {
+    recnt = setTimeout(() => {
         connect();
     }, 60000);
 }
@@ -339,32 +319,32 @@ function err(e){
     e = e.toString();
     if (e){
         clearInterval(query);
-        if(!~e.indexOf('ECONNREFUSED')){
+        if (!~e.indexOf('ECONNREFUSED')){
             adapter.log.error("LG TV " + e);
             adapter.log.error('Error socket: Reconnect after 15 sec...');
             adapter.setState('info.connection', false, true);
             connection = false;
-            setTimeout(
-                function (){
-                    main();
-                }, 15000);
+            setTimeout(() => {
+                //main();
+                connect();
+            }, 15000);
         }
     }
 }
 
 function CreatObject(){
-    var arr = [];
-    var interval, t = 1000;
-    adapter.getState('remote.0', function (err, state){
+    let arr = [];
+    let interval, t = 1000;
+    adapter.getObject('remote.0', (err, state) => {
         if ((err || !state)){
-            for (var key in REMOTE_CMDS) {
-                if(REMOTE_CMDS.hasOwnProperty(key)){
+            for (let key in REMOTE_CMDS) {
+                if (REMOTE_CMDS.hasOwnProperty(key)){
                     arr.push(key);
                 }
             }
-            arr.forEach(function(cmd, i) {
+            arr.forEach((cmd, i) => {
                 interval = t * i;
-                setTimeout(function() {
+                setTimeout(() => {
                     adapter.setObject('remote.' + cmd, {
                         type:   'state',
                         common: {
@@ -382,6 +362,8 @@ function CreatObject(){
     });
 }
 
-/*
-
- */
+if (module.parent){
+    module.exports = startAdapter;
+} else {
+    startAdapter();
+}
